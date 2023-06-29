8e71d1bf0b7231bde86502a5ed9bc971ea1a51d0
AI_13_장기훈 <102294660+todd-jang@users.noreply.github.com>
2023-06-29 17:12:00 +0900

    6/28 packageGoods

---
+++ eclipse-workspace/java_study/src/main/java/mypackage/Goods2.java
@@ -0,0 +1,12 @@
+package mypackage;
+
+public class Goods2 {
+	public String name;   //모든 접근가능
+	protected int price;  // 같은 패키지 + 자식접근*
+	int countStock;
+	private int countSold;
+	
+	public void m() {
+		countSold=0;
+	}
+}
---
+++ eclipse-workspace/java_study/src/main/java/mypackage/Goods2App.java
@@ -0,0 +1,21 @@
+package mypackage;
+
+import chapter03.Goods2;
+
+public class Goods2App {
+	
+	public static void man(String[] args) {
+		
+		Goods2 g= new Goods2();
+		
+		g.name = "camera";
+		
+		//g.count.Sold =10;
+		
+		//g.price=1000;
+		
+		//디폴트는 같은 패키지에서 접근 가능
+		//g.countStock =20;
+		
+		//g.countSold=10;
+}
