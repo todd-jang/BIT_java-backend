3c335e9d7e03953503a93bfb96c561eab8943cd7
AI_13_장기훈 <102294660+todd-jang@users.noreply.github.com>
2023-06-29 14:03:30 +0900

    6/29

---
+++ eclipse-workspace/java_study/src/main/java/chapter04/StringTest02.java
@@ -0,0 +1,23 @@
+package chapter04;
+
+public class StringTest02 {
+	
+	public static void main(String[] args) {
+		String s1="a";
+		String s2="d";
+		Stirng s3= s2;
+		
+//		Sysyem.out.pringln(s1.toUpperCase());
+		s2=s1.toUpperCase();
+		System.out.println(s2);
+		
+		String s4=s2.concat("??");//->ABC??
+		String s5="!".concat(s2).concat("@");//!ABC@
+	}
+	public static boolean equalsHello(String s) {
+		//return S.equalsHello("Hello");보다는
+	  return "Hello".equals(s);
+		
+	}
+
+}
