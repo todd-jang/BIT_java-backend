d1d71b274b08922a13ebcf5ed4620f98047fa7ca
AI_13_장기훈 <102294660+todd-jang@users.noreply.github.com>
2023-06-29 13:02:14 +0900

    6/28

---
+++ eclipse-workspace/practice1/src/practice1/GoodsApp.java
@@ -0,0 +1,23 @@
+import java.util.*;//Scanner사용
+
+package practice1;
+
+public class Prob1 {
+	public static void main(String[] args) {
+		
+		Scanner scanner=new Scanner(System.in);
+		
+		Goods[] goods=new goods[COUNT_GOODS];
+		
+		//상품
+		for (int=0; i<COUNT_GOODS; i++) {
+			String line = scanner.nextLine();
+			String[] info=line.split(" ");
+			String name=info[0];
+			int price = Interger.parseInt(info[1]);
+			int count = Interger.parseInt(info[2]);
+		}
+				
+	}
+
+}
---
+++ eclipse-workspace/practice1/src/practice1/Prob1.java
@@ -0,0 +1,7 @@
+package prob1;
+
+public class Prob1 {
+	public static void main(String[] args) {
+	}
+}
+
