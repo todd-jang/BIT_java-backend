8e7913df7b5535155ebed6d1d312a82686483e12
AI_13_장기훈 <102294660+todd-jang@users.noreply.github.com>
2023-12-26 17:40:05 +0900

    multi19 java기초 HelloWorld

---
+++ eclipse-workspace/HelloWorld/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,16 @@
+package HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//      System.out.println("Hello, World!");
+		double Num1=2.2;
+		float Num2=0.9f;
+//		int Num3= (int)Num1 + (int)Num2;
+		int Num4= (int)(Num1 + Num2);
+//		System.out.println(Num3);
+		System.out.println(Num4);
+	}
+
+}
